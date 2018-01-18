1518f739f7a848fa72cd9deae41c3626e565ce77
an1m4l <rjqueiros@gmail.com>
2018-01-18 12:29:10 +0000

    another waste of space

---
+++ app/src/main/java/an1m4l/anothergame/MainActivity.java
@@ -0,0 +1,28 @@
+package an1m4l.anothergame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), Story1.class);
+                startActivity(startIntent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/an1m4l/anothergame/Story2.java
@@ -0,0 +1,13 @@
+package an1m4l.anothergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Story2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story2);
+    }
+}
