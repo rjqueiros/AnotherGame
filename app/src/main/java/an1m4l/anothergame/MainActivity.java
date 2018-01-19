78cc3213d748a1a86a507ef04076db635115d155
an1m4l <rjqueiros@gmail.com>
2018-01-19 21:09:04 +0000

    another waste of space

--- app/src/main/java/an1m4l/anothergame/MainActivity.java
+++ app/src/main/java/an1m4l/anothergame/MainActivity.java
@@ -1,28 +1,42 @@
 package an1m4l.anothergame;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonStart = findViewById(R.id.buttonStart);
+        SharedPreferences sharedPref = getSharedPreferences("playerInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor =sharedPref.edit();
+        editor.putString("playerWPN", "Knife");
+        editor.putInt("playerHP", 10);
+        editor.apply();
 
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), Story1.class);
-                startActivity(startIntent);
+            Button buttonStart = findViewById(R.id.buttonStart);
+            buttonStart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent startIntent = new Intent(getApplicationContext(), Story1.class);
+                    startActivity(startIntent);
 
-            }
-        });
-
+                }
+            });
     }
+
+
+
+
+
+
 }
--- app/src/main/java/an1m4l/anothergame/Story1.java
+++ app/src/main/java/an1m4l/anothergame/Story1.java
@@ -1,13 +1,30 @@
 package an1m4l.anothergame;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 
 public class Story1 extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story1);
+
+        SharedPreferences sharedPref = getSharedPreferences("playerInfo", Context.MODE_PRIVATE);
+        String playerWPN = sharedPref.getString("playerWPN", "");
+        int playerHP = sharedPref.getInt("playerHP", 0);
+
+        TextView txtStory1 = findViewById(R.id.txtStory1);
+        txtStory1.setText("hp: " + playerHP + "\nweapon: " + playerWPN);
+
     }
+
 }
